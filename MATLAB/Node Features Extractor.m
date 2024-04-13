25315a957d0d7189e842c9d8f05fb1dbbe66a8c4
Abdulrazaq Sanni <165695047+walerazaq@users.noreply.github.com>
2024-04-13 20:20:01 +0100

    Update file paths

--- MATLAB/Node Features Extractor.m
+++ MATLAB/Node Features Extractor.m
@@ -4,21 +4,23 @@ clear; close all;
 
 addpath(genpath('/mnt/scratch2/users/asanni/MatCodes/'));
 
-freqBand = 'Alpha';
+filepath = 'MEG';
 
-switch freqBand
-    case 'Alpha'
-        data_path = '/mnt/scratch2/users/asanni/Alpha/';
-    case 'Beta'
-        data_path = '/mnt/scratch2/users/asanni/Beta/';
-    case 'Delta'
-        data_path = '/mnt/scratch2/users/asanni/Delta/';
-    case 'Theta'
-        data_path = '/mnt/scratch2/users/asanni/Theta/';
-    case 'Gamma'
-        data_path = '/mnt/scratch2/users/asanni/Gamma/';
-    case 'overAll'
-        data_path = '/mnt/scratch2/users/asanni/Overall/';
+switch filepath
+    case 'EEGAlpha'
+        data_path = '/mnt/scratch2/users/asanni/EEG/AlphaTrials/';
+    case 'EEGBeta'
+        data_path = '/mnt/scratch2/users/asanni/EEG/Beta/';
+    case 'EEGDelta'
+        data_path = '/mnt/scratch2/users/asanni/EEG/Delta/';
+    case 'EEGTheta'
+        data_path = '/mnt/scratch2/users/asanni/EEG/Theta/';
+    case 'EEGGamma'
+        data_path = '/mnt/scratch2/users/asanni/EEG/Gamma/';
+    case 'EEGOverAll'
+        data_path = '/mnt/scratch2/users/asanni/EEG/Overall/';
+    case 'MEG'
+        data_path = '/mnt/scratch2/users/asanni/MEG/Training Data/';
 end
 
 mat_files = dir([data_path,'*.mat']);
@@ -31,6 +33,7 @@ strengths_results = cell(numel(mat_files), 1);
 activity_results = cell(numel(mat_files), 1);
 mobility_results = cell(numel(mat_files), 1);
 complexity_results = cell(numel(mat_files), 1);
+betweenness_results = cell(numel(mat_files), 1);
 
 %% Start parfor loop
 parfor idx = 1:numel(mat_files)
@@ -42,21 +45,26 @@ parfor idx = 1:numel(mat_files)
     data_cur = load([data_path,mat_files(idx).name]);
 
     % 1. Get Efficiency
-    efficiency_results{idx} = efficiency_wei(data_cur.freqMatrix,2);
+    efficiency_results{idx} = efficiency_wei(data_cur.BCM,2);
 
     % 2. Get Clustering Coefficients
-    clusteringcoef_results{idx} = clustering_coef_wu(data_cur.freqMatrix);
+    clusteringcoef_results{idx} = clustering_coef_wu(data_cur.BCM);
 
     % 3. Get Strengths
-    strengths_results{idx} = strengths_und(data_cur.freqMatrix);
+    strengths_results{idx} = strengths_und(data_cur.BCM);
 
     % 4. Get Hjorth Parameters
-    [ACTIVITY, MOBILITY, COMPLEXITY] = hjorth(data_cur.freqMatrix,0);
+    [ACTIVITY, MOBILITY, COMPLEXITY] = hjorth(data_cur.BCM,0);
 
     % Store Hjorth Parameters
     activity_results{idx} = ACTIVITY;
     mobility_results{idx} = MOBILITY;
     complexity_results{idx} = COMPLEXITY;
+
+    % 5. Get Betweenness
+    W = weight_conversion(data_cur.BCM, 'lengths');
+    betweenness_results{idx} = betweenness_wei(W);
+
 end
 
 %% Save results from outside the parfor loop
@@ -84,4 +92,8 @@ for idx = 1:numel(mat_files)
     
     COMPLEXITY = complexity_results{idx};
     save([data_path, 'Complexity/', file, '.mat'], 'COMPLEXITY', '-v7.3', '-nocompression');
+
+    % Save Betweenness
+    betweenness = betweenness_results{idx};
+    save([data_path, 'Betweenness/', file, '.mat'], 'betweenness', '-v7.3', '-nocompression');
 end
